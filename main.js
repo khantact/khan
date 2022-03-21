f36560177eb8001589f8a79ecdd93fd0ab174aac
Kevin Han <khan@colgate.edu>
2022-03-21 14:02:07 -0400

    Navbar animation / fixed back to top arrow

---
+++ main.js
@@ -0,0 +1,32 @@
+let navbar = document.querySelector(".navbar");
+let vh = Math.round(window.innerHeight / 100);
+window.onscroll = () =>{
+    navToOpaque();
+}
+
+// window.onload = function(){
+// if (window.jQuery){
+//     alert('yes');
+// } else{
+//     alert('no');
+// }
+// }
+
+function navToOpaque(){
+    if (window.pageYOffset > vh*100){
+        document.querySelector('.navbar').style.display= "none";
+        $('.navbar').fadeIn("fast");
+        document.querySelector('.navbar').style.background="rgb(4, 7, 17)";
+    } else {
+        navAppear()
+        document.querySelector('.navbar').style.background = 'rgba(37, 50, 55, 0)';
+    }
+}
+function navDis(){
+    document.querySelector('.navbar').style.visibility = 'hidden';
+}
+
+function navAppear(){
+    $('.navbar').fadeIn("fast");
+    document.querySelector('.navbar').style.visibility = 'visible';
+}
